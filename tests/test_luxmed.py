b7090efd9bd4f1078590bab7247d4c4940b485a2
Przemysław Palacz <pprzemal@gmail.com>
2019-08-27 08:45:51 +0200

    Reuse identical test responses

--- tests/test_luxmed.py
+++ tests/test_luxmed.py
@@ -18,29 +18,29 @@ def test_authentication(luxmed):
     luxmed._transport.authenticate()
 
 
-@pytest.mark.vcr()
+@pytest.mark.vcr('cities_languages.yaml')
 def test_warsaw_city(luxmed, from_date):
     assert luxmed.cities(from_date=from_date)[1] == 'Warszawa'
 
 
-@pytest.mark.vcr()
+@pytest.mark.vcr('cities_languages.yaml')
+def test_english_language(luxmed, from_date):
+    assert luxmed.languages(from_date=from_date)[11] == 'english'
+
+
+@pytest.mark.vcr('city_clinics_services.yaml')
 def test_warsaw_clinic(luxmed, from_date):
     assert luxmed.clinics(city_id=1, from_date=from_date)[1] == 'LX Warszawa - Al. Jerozolimskie 65/79'
+
+
+@pytest.mark.vcr('city_clinics_services.yaml')
+def test_warsaw_allergist_service(luxmed, from_date):
+    assert luxmed.services(city_id=1, from_date=from_date)[4387] == 'Konsultacja alergologa'
 
 
 @pytest.mark.vcr()
 def test_warsaw_allergist_doctor(luxmed, from_date):
     assert luxmed.doctors(city_id=1, service_id=4387, from_date=from_date)[16772].startswith('HANNA')
-
-
-@pytest.mark.vcr()
-def test_english_language(luxmed, from_date):
-    assert luxmed.languages(from_date=from_date)[11] == 'english'
-
-
-@pytest.mark.vcr()
-def test_warsaw_allergist_service(luxmed, from_date):
-    assert luxmed.services(city_id=1, from_date=from_date)[4387] == 'Konsultacja alergologa'
 
 
 @pytest.mark.vcr()
